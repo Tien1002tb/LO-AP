eb171efb910061ad4a236c6cca95874819fd4fc7
Tien1002tb <godsofmickey@gmail.com>
2024-04-17 22:02:34 +0700

    17/4 10:02
    cap nhat doc nhiet do kieu float

--- nutnhan/main.c
+++ nutnhan/main.c
@@ -14,10 +14,11 @@
 GPIO_InitTypeDef GPIO_Structure; 
 int congtru_tong =0 ;
 uint8_t RHI = 0,RHD = 0 , TCI = 0, TCD = 0;
-u8 RHI_set = 0, TCI_set = 0;
+float RHI_set = 50.00, TCI_set = 50.00;
 int count = 0;
 char Temp1[20], Temp2[20],Temp3[20];
 char Humi1[20], Humi2[20],Humi3[20];
+float temp , humi;
 
 void gpio_Init(void);
 void mainmenu(void);
@@ -84,43 +85,103 @@ void mainmenu(void){
 				}		
 		}
 
+//void mannhietdo(void){
+//	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
+//	sprintf(Temp1 ,"T:%02u *C",TCI);
+//	lcd_i2c_msg(1 ,1, 0, Temp1);
+//	sprintf(Humi1 ,"H:%02u %% >" ,RHI);
+//	lcd_i2c_msg(1 ,2, 0, Humi1);
+//	DelayMs(50);
+//	}
+//void mansetting1(void){
+//	sprintf(Temp2 ,"SET >T:%02u  ", TCI_set);
+//	lcd_i2c_msg(1 ,1, 0, Temp2);
+//	lcd_i2c_msg(1 ,1, 10, "*C");
+//	sprintf(Humi1 ,"H:%02u %% " ,RHI);
+//	lcd_i2c_msg(1 ,2, 5, Humi1);
+//	DelayMs(50);
+//}
+
+
+//void setting(void){
+//		mansetting1();   
+//		if (congtru_tong == 2 && GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang nhiet do 
+//				TCI_set = TCI_set + 1;
+//					if (TCI_set >= 90){
+//							TCI_set = 0;
+//					}		
+//			}
+//		else if (congtru_tong == 2 && GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam nhiet do 
+//				TCI_set = TCI_set - 1;
+//					if (TCI_set == 0){
+//						TCI_set = 90;
+//					}
+//			}
+//		if (TCI < TCI_set){
+//			GPIO_SetBits(GPIO_PORT, DEN);
+//			GPIO_ResetBits(GPIO_PORT, QUAT);
+//		}
+//	else if (TCI > TCI_set){
+//			GPIO_ResetBits (GPIO_PORT, DEN);
+//			GPIO_SetBits (GPIO_PORT, QUAT);
+//		}
+//}
+
+
+//void mannhietdo(void){
+//	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
+//	sprintf(Temp1 ,"T:%02u *C",TCI);
+//	lcd_i2c_msg(1 ,1, 0, Temp1);
+//	sprintf(Humi1 ,"H:%02u %% >" ,RHI);
+//	lcd_i2c_msg(1 ,2, 0, Humi1);
+//	DelayMs(50);
+//	}
+//void mansetting1(void){
+//	sprintf(Temp2 ,"SET >T:%02u  ", TCI_set);
+//	lcd_i2c_msg(1 ,1, 0, Temp2);
+//	lcd_i2c_msg(1 ,1, 10, "*C");
+//	sprintf(Humi1 ,"H:%02u %% " ,RHI);
+//	lcd_i2c_msg(1 ,2, 5, Humi1);
+//	DelayMs(50);
+//}
+
 void mannhietdo(void){
 	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
-	sprintf(Temp1 ,"T:%02u *C",TCI);
+ temp = TCI + (float)TCD / 100;
+	humi = RHI + (float)RHD / 100;
+	sprintf(Temp1 ,"T:%.2f *C", temp); 
 	lcd_i2c_msg(1 ,1, 0, Temp1);
-	sprintf(Humi1 ,"H:%02u %% >" ,RHI);
+	sprintf(Humi1 ,"H:%.2f %% >" ,humi);
 	lcd_i2c_msg(1 ,2, 0, Humi1);
 	DelayMs(50);
 	}
 void mansetting1(void){
-	sprintf(Temp2 ,"SET >T:%02u  ", TCI_set);
+	sprintf(Temp2 ,"SET >T:%.2f  ", TCI_set);
 	lcd_i2c_msg(1 ,1, 0, Temp2);
-	lcd_i2c_msg(1 ,1, 10, "*C");
-	sprintf(Humi1 ,"H:%02u %% " ,RHI);
+	lcd_i2c_msg(1 ,1, 13, "*C");
+	sprintf(Humi1 ,"H:%.2f %% " ,RHI_set);
 	lcd_i2c_msg(1 ,2, 5, Humi1);
 	DelayMs(50);
 }
-
-
 void setting(void){
 		mansetting1();   
 		if (congtru_tong == 2 && GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang nhiet do 
-				TCI_set = TCI_set + 1;
+				TCI_set = TCI_set + 0.1;
 					if (TCI_set >= 90){
 							TCI_set = 0;
 					}		
 			}
 		else if (congtru_tong == 2 && GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam nhiet do 
-				TCI_set = TCI_set - 1;
+				TCI_set = TCI_set - 0.5;
 					if (TCI_set == 0){
 						TCI_set = 90;
 					}
 			}
-		if (TCI < TCI_set){
+		if (temp < TCI_set){
 			GPIO_SetBits(GPIO_PORT, DEN);
 			GPIO_ResetBits(GPIO_PORT, QUAT);
 		}
-	else if (TCI > TCI_set){
+	else if (temp > TCI_set){
 			GPIO_ResetBits (GPIO_PORT, DEN);
 			GPIO_SetBits (GPIO_PORT, QUAT);
 		}
